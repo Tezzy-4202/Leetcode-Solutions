f03c5adeb9553a980da402dfe8764cc55a6fe092
Arun Teja <97935516+PRED263@users.noreply.github.com>
2025-06-10 16:34:37 +0530

    Create 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI

---
+++ 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI/3753-MaximumDifferenceBetweenEvenAndOddFrequencyI.cpp
@@ -0,0 +1,14 @@
+// Last updated: 6/10/2025, 4:34:26 PM
+class Solution {
+public:
+    int maxDifference(string s) {
+        vector<int> mpp(26);
+        int maxi = 0, mini = s.size();
+        for (char c : s) mpp[c - 'a']++;
+        for (int i = 0 ; i < 26; i++) {
+            if (mpp[i] % 2 != 0) maxi = max(maxi, mpp[i]);
+            if (mpp[i] % 2 == 0 && mpp[i] > 0) mini = min(mini, mpp[i]);
+        } 
+        return maxi - mini;
+    }
+};
