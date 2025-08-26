a5caa6d16505190c1e6328954bb1cd74af11fe90
Arun Teja <97935516+PRED263@users.noreply.github.com>
2025-08-26 23:43:44 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/26/2025, 11:44:02 PM
+class Solution {
+public:
+    int c2n(char a){
+        switch(a){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    int romanToInt(string s) {
+     int result=0;
+     for(int i=0;i<s.length();i++){
+        if (i+1<s.length() && c2n(s[i])<c2n(s[i+1])){
+            result -=c2n(s[i]);
+        }
+        else {
+            result +=c2n(s[i]);
+        }
+     }
+     return result;
+    }
+};
