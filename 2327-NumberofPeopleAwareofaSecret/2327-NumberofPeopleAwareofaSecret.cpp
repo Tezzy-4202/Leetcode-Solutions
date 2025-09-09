cf962d6efe350326092f557a70e37e11ce8e5ed5
Arun Teja <97935516+Tezzy-4202@users.noreply.github.com>
2025-09-09 23:20:33 +0530

    File updated at 09/09/2025, 23:20:43

--- 2327-NumberofPeopleAwareofaSecret/2327-NumberofPeopleAwareofaSecret.cpp
+++ 2327-NumberofPeopleAwareofaSecret/2327-NumberofPeopleAwareofaSecret.cpp
@@ -1,9 +1,8 @@
-// Last updated: 09/09/2025, 23:18:57
+// Last updated: 09/09/2025, 23:20:43
 class Solution {
 public:
     int mod = (long)(1000000007);
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        // This variable tracks the running count of people who are able to share the secret.
         long noOfPeopleWithSecret = 0;
         
         // dp[i] stores the number of people who learn the secret on day i.
@@ -11,12 +10,12 @@ public:
         dp[1] = 1;
         
         for(int i=2; i<=n; i++){
-            // People who learned 'delay' days ago start sharing today.
+            //start sharing today.
             long noOfNewPeopleWithSecret = 0;
             if((i-delay)>=1)
                 noOfNewPeopleWithSecret = dp[i-delay];
             
-            // People who learned 'forget' days ago forget today and stop sharing.
+            //forget today and stop sharing.
             long noOfPeopleForgetSecret = 0;
             if((i-forget)>=1)
                 noOfPeopleForgetSecret = dp[i-forget];
@@ -24,12 +23,9 @@ public:
             // Update the count of active sharers.
             noOfPeopleWithSecret = (noOfPeopleWithSecret + noOfNewPeopleWithSecret - noOfPeopleForgetSecret + mod) % mod;
             
-            // The number of new people learning today equals the number of people sharing.
             dp[i] = noOfPeopleWithSecret;
         }
         
-        // The total number of people aware on day 'n' are those who learned
-        // in the window [n - forget + 1, n], as they haven't forgotten yet.
         long ans=0;
         for(int i=n-forget+1; i<=n; i++){
             ans = (ans+dp[i])%mod;
